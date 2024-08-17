d1d16bf6d79ccb0eb2106b7bdee41db82c290580
Aayush_Raj <98972400+aprogramme@users.noreply.github.com>
2024-08-17 17:02:12 +0000

    operations

--- 01_Basics/03_conversionsAndOperations.js
+++ 01_Basics/03_conversionsAndOperations.js
@@ -1,3 +1,5 @@
+// *********************************** Conversions ***********************************
+
 // let score = "33avc"
 // let score = null
 // let score = undefined
@@ -31,3 +33,46 @@ let someNumber = 22
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+// *********************************** Operations ***********************************
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello "
+let str2 = "Aayush"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 1);
+
+// console.log(true);
+// console.log(+true);
+// console.log(false);
+// console.log(+false);
+
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;  // prefix
+gameCounter++;  // postfix
+console.log(gameCounter);
